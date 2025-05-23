0c29576877243b4149a72f0eceaf2aaeb480bf2e
Rodrigo Freitas <rsfreitas@tec-ci.com.br>
2025-05-23 00:14:42 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+googleKey = os.getenv('GOOGLE_API_KEY')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'news.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Importe as rotas DEPOIS da inicialização do 'app' e 'db'
+from routes.views import *
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,12 @@
+from app import db # Importa o objeto db do seu app.py
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10), nullable=False) # Formato YYYY-MM-DD
+    image = db.Column(db.String(255), nullable=True)
+    title = db.Column(db.String(255), nullable=False)
+    summary = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<News {self.title}>'
